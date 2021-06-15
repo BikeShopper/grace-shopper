3efd0e2b656ebc216f68c70f65029707d78f3584
Leo P <leon86ka@gmail.com>
2021-06-15 17:13:06 -0400

    console.logs removed

--- server/api/bikes.js
+++ server/api/bikes.js
@@ -4,9 +4,7 @@ const Bike = require('../db/models/bike');
 // GET /api/bikes
 router.get('/', async (req, res, next) => {
   try {
-    console.log('in API');
     const bikes = await Bike.findAll();
-    console.log('BIKes', bikes);
     res.json(bikes);
   } catch (error) {
     next(error);
