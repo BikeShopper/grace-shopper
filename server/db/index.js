32a0f0b63825c122ffa64a9d8fc991b8943b556e
Luis Rodriguez <rodriguezlf4@gmail.com>
2021-06-16 10:31:04 -0700

    create userCart model

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,10 @@
 //this is the access point for all things database related!
 
-const db = require('./db');
+const db = require("./db");
 
-const User = require('./models/user');
-const Bike = require('./models/bike');
+const User = require("./models/user");
+const Bike = require("./models/bike");
+const UserCart = require("./models/userCart");
 
 //associations could go here!
 
@@ -12,10 +13,11 @@ module.exports = {
   models: {
     User,
     Bike,
+    UserCart,
   },
 };
 
 User.hasOne(Bike);
-
 Bike.belongsTo(User);
-
+User.hasOne(UserCart);
+UserCart.belongsTo(User);
