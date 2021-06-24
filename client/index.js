e53365bd6d774edc253b4bfec345db1e5430b14d
Leo P <leon86ka@gmail.com>
2021-06-24 03:03:53 -0400

    added Material UI

--- client/index.js
+++ client/index.js
@@ -4,11 +4,13 @@ import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import history from './history';
 import store from './store';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import App from './App';
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
+      <CssBaseline />
       <App />
     </Router>
   </Provider>,
